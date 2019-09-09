5da2246116326b6ce22c218581ff9ecec7312425
proogramer <princerupela67@gmail.com>
2019-09-09 14:22:41 +0530

    add article using mongodb

--- app.js
+++ app.js
@@ -1,67 +1,79 @@
 const express =require('express');
 const path = require('path');
+const mongoose =require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost/node-crud-operarion');
+let db = mongoose.connection;
+
+//check connection
+db.once('open',function(){
+   console.log('connection to mongo db');
+});
+
+//check for DB errors
+db.on('error',function(err){
+    console.log(err);
+});
 
 // Init App
 const app =express();
+
+//Bring in Modes
+let Articles = require('./models/article');
 
 //Load view Engine
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','pug');
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 
 // Home Route
 app.get('/',function(req,res){
-    res.render('index',{
-        title:'articles'
+    Articles.find({},function(err,articles){
+        console.log(articles);
+        if(err){
+            console.log(err);
+        }else{
+            res.render('index',{
+                title:'Add Articles',
+                articles:articles
+            });
+        }
+
     });
 });
 
 // add articles routes
-
 app.get('/articles/add',function(req,res){
-let articles =[
-    {
-        id:1,
-        title:'Artice one',
-        author:'Prince',
-        body:'this is article one'
-    },
-    {
-        id:2,
-        title:'Artice two',
-        author:'Prince',
-        body:'this is article two'
-    },
-    {
-        id:3,
-        title:'Artice three',
-        author:'Prince',
-        body:'this is article three'
-    },
-    {
-        id:4,
-        title:'Artice four',
-        author:'Prince',
-        body:'this is article four'
-    },
-    {
-        id:5,
-        title:'Artice five',
-        author:'Prince',
-        body:'this is article five'
-    },
-    {
-        id:6,
-        title:'Artice six',
-        author:'Prince',
-        body:'this is article six'
-    },
-];
     res.render('add_article',{
         title:'Add Articles',
-        articles:articles
     });
+
+});
+
+app.post('/articles/add',function(req,res){
+    let article = new Articles();
+    article.title =req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save(function(err){
+        if(err){
+            console.log(err);
+            return ;
+        }else{
+            res.redirect('/');
+        }
+    })
+
+
 });
 
 //Start Server
